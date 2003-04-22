87e9cd84b80b8ecdfc82b94fb5b6df3b2207689b
chris <chris@94779f63-8a0d-0410-8a4e-8732221620d0>
2003-04-22 08:10:18 +0000

    remove bsd-tunnel stuff (doesn't belong here), amend to only need AS number
    git-svn-id: file:///Volumes/Backup Macintosh HD/Users/chris/from_munkyii/ss-repo/svn/funknet-tools/trunk@6 94779f63-8a0d-0410-8a4e-8732221620d0

--- generate-tuns.pl
+++ generate-tuns.pl
@@ -4,14 +4,12 @@ use lib './lib';
 use Data::Dumper;
 use Funknet::Config::Whois;
 
-unless (scalar @ARGV == 3) {
-    print STDERR "usage: $0 local_as local_os doit";
+unless (scalar @ARGV == 1) {
+    print STDERR "usage: $0 local_as\n";
     exit(1);
 }
 
 my $local_as = $ARGV[0];
-my $local_os = $ARGV[1];
-my $doit = $ARGV[2];
 
 my $whois = Funknet::Config::Whois->new();
 my $local_tun = $whois->tunnels($local_as);
@@ -22,25 +20,4 @@ for my $tun (@{$local_tun}) {
     print "\n";
 }
 
-print Dumper $local_bgp;
 print $local_bgp->config;
-
-sub tunnelup_bsd {
-    my ($tun) = @_;
-
-    my ($destroy, $create, $endpoints, $addresses);
-
-    if ($tun->{type} eq 'ipip') {
-	
-	$destroy = "ifconfig gif$tun->{index} destroy";
-	$create = "ifconfig gif$tun->{index} create";
-	$endpoints = "ifconfig gif$tun->{index} tunnel $tun->{local_endpoint} $tun->{remote_endpoint} mtu 1480";
-	$addresses = "ifconfig gif$tun->{index} inet $tun->{local_address} $tun->{remote_address} netmask 255.255.255.252";
-    }
-    
-    if ($doit) {
-    } else {
-	print $destroy, "\n", $create, "\n", $endpoints, "\n", $addresses, "\n";
-    }
-}
-
