1d1976793f07e8af2c1b5f7b00fcb9104a21afa0
chris <chris@94779f63-8a0d-0410-8a4e-8732221620d0>
2003-04-23 20:50:35 +0000

    won't have to stay a placeholder for long...
    git-svn-id: file:///Volumes/Backup Macintosh HD/Users/chris/from_munkyii/ss-repo/svn/funknet-tools/trunk@21 94779f63-8a0d-0410-8a4e-8732221620d0

--- lib/Funknet/Config/CLI/Secrets.pm
+++ lib/Funknet/Config/CLI/Secrets.pm
@@ -5,9 +5,18 @@ use vars qw/ $username $password $enable /;
 # this is pretty much a placeholder for a decent
 # way of holding on to usernames/passwords
 
-$username = { '213.210.34.174' => '' };
-$password = { '213.210.34.174' => 'funknet' };
-$enable   = { '213.210.34.174' => '' };
+$username = { 
+    '213.210.34.174' => '',
+    '127.0.0.1'      => '',
+};
+$password = { 
+    '213.210.34.174' => 'funknet',
+    '127.0.0.1'      => 'zebra',
+};
+$enable   = { 
+    '213.210.34.174' => '',
+    '127.0.0.1'      => '',
+};
 
 sub username {
     my ($class, $host) = @_;
