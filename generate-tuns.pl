0a1c0cb3d1da03ec512d381eb3bace094e5c4252
chris <chris@94779f63-8a0d-0410-8a4e-8732221620d0>
2003-04-24 21:32:30 +0000

    update docs (!) and driver script.
    git-svn-id: file:///Volumes/Backup Macintosh HD/Users/chris/from_munkyii/ss-repo/svn/funknet-tools/trunk@38 94779f63-8a0d-0410-8a4e-8732221620d0

--- generate-tuns.pl
+++ generate-tuns.pl
@@ -5,17 +5,17 @@ use Data::Dumper;
 use Funknet::Config::Whois;
 use Funknet::Config::Host;
 
-unless (scalar @ARGV == 1) {
-    print STDERR "usage: $0 local_as\n";
+unless (scalar @ARGV == 4) {
+    print STDERR "usage: $0 local_as local_router local_os local_host\n";
     exit(1);
 }
 
 my $local_as = $ARGV[0];
 
 my $whois = Funknet::Config::Whois->new( local_as => $ARGV[0],
-					 local_os => 'ios',
-					 local_router => 'ios',
-					 local_host => '213.210.34.174',
+					 local_os => $ARGV[2],
+					 local_router => $ARGV[1],
+					 local_host => $ARGV[3],
 				       );
 my $whois_tun = $whois->tunnels;
 my $whois_bgp = $whois->sessions;
@@ -27,9 +27,9 @@ for my $tun (@{$whois_tun}) {
 print $whois_bgp->config;
 
 my $host = Funknet::Config::Host->new( local_as => $ARGV[0],
-				       local_os => 'ios',
-				       local_router => 'ios',
-				       local_host => '213.210.34.174',
+				       local_os => $ARGV[2],
+				       local_router => $ARGV[1],
+				       local_host => $ARGV[3],
 				     );
 my $host_tun = $host->tunnels;
 my $host_bgp = $host->sessions;
