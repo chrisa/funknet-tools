2de55ae3ad06b13fc43f36b1b3c9fef904655d68
chris <chris@94779f63-8a0d-0410-8a4e-8732221620d0>
2003-04-28 19:22:29 +0000

    if we have an access list from whois and from the host, replace the host one with the whois one. this will mean that we bounce sessions too much...
    git-svn-id: file:///Volumes/Backup Macintosh HD/Users/chris/from_munkyii/ss-repo/svn/funknet-tools/trunk@44 94779f63-8a0d-0410-8a4e-8732221620d0

--- lib/Funknet/Config/BGP/IOS.pm
+++ lib/Funknet/Config/BGP/IOS.pm
@@ -112,10 +112,14 @@ sub diff {
 		push @cmds, "no ip prefix-list ".$h_n->{_acl_out}->name;
 	    }
 	    if (defined $n->{_acl_in} && defined $h_n->{_acl_in}) {
-		push @cmds, $n->{_acl_in}->diff($h_n->{_acl_in});
+		push @cmds, "no route-map ".$h_n->{_acl_in}->name;
+		push @cmds, "no ip prefix-list ".$h_n->{_acl_in}->name;
+		push @cmds, $n->{_acl_in}->config;
 	    }
 	    if (defined $n->{_acl_out} && defined $h_n->{_acl_out}) {
-		push @cmds, $n->{_acl_out}->diff($h_n->{_acl_out});
+		push @cmds, "no route-map ".$h_n->{_acl_out}->name;
+		push @cmds, "no ip prefix-list ".$h_n->{_acl_out}->name;
+		push @cmds, $n->{_acl_out}->config;
 	    }
 	}
 	push @bounce_req, $n->remote_addr;
