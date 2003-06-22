8b6906b5e1fb54ba6a4aefb8fe925971072d10dc
chris <chris@94779f63-8a0d-0410-8a4e-8732221620d0>
2003-06-22 18:06:12 +0000

    add multi-valued keys. see pod for semantics
    git-svn-id: file:///Volumes/Backup Macintosh HD/Users/chris/from_munkyii/ss-repo/svn/funknet-tools/trunk@78 94779f63-8a0d-0410-8a4e-8732221620d0

--- lib/Funknet/Config/ConfigFile.pm
+++ lib/Funknet/Config/ConfigFile.pm
@@ -3,6 +3,42 @@ use strict;
 use Data::Dumper;
 use vars qw/ $AUTOLOAD /;
 
+
+=head1 NAME
+
+Funknet::Config::ConfigFile
+
+=head1 SYNOPSIS
+
+  my $config = Funknet::Config::ConfigFile->new( $configfile )
+
+=head1 DESCRIPTION
+
+  An abstraction over a simple test config file. Syntax is:
+
+    key = value
+    key = value, value, value
+
+=head1 METHODS
+
+=head2 new
+
+Call with one arg of the full path to the config file. Returns a
+config object which you can use to access keys. 
+
+=head2 AUTOLOAD
+
+To retrieve a key, call ->key on the ConfigFile object, or as a class
+method. Multi-value key semantics: called in scalar context, returns
+the value, or the first value of a list. Called in list context,
+returns either the list or just the on value. Never returns a
+reference.
+
+=cut
+
+
+my $config;
+
 sub new {
     my ($class, $file) = @_;
     my $self = bless {}, $class;
@@ -17,11 +53,14 @@ sub new {
 	$key =~ s/\s+$//;
 	$values =~ s/^\s+//;
 	$values =~ s/\s+$//;
-#	$self->{config}->{$key} = [ split /,/,$values ];
-	$self->{config}->{$key} = $values;
+	if ($values =~ /,/) {
+	    $config->{$key} = [ split /\s*,\s*/,$values ];
+	} else {
+	    $config->{$key} = $values;
+	}
     }
-
     close CONF;
+    $self->{config} = $config;
     return $self;
 }
 
@@ -29,8 +68,20 @@ sub AUTOLOAD {
     my ($self) = @_;
     my $key = $AUTOLOAD;
     $key =~ s/Funknet::Config::ConfigFile:://;
-    if (exists $self->{config}->{$key}) { 
-	return $self->{config}->{$key};
+    if (ref $self) {
+	$config = $self->{config};
+    }
+	
+    if (exists $config->{$key}) { 
+	if (ref $config->{$key}) {
+	    if (wantarray) {
+		return @{ $config->{$key} };
+	    } else {
+		return $config->{$key}->[0];
+	    }
+	} else {
+	    return $config->{$key};
+	}
     } else {
 	warn "accessing non existent config param $key";
 	return undef;
