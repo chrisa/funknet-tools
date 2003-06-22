f8643d44b16d82d80bc13336a445de485ff1a12f
chris <chris@94779f63-8a0d-0410-8a4e-8732221620d0>
2003-06-22 17:32:13 +0000

    test script
    git-svn-id: file:///Volumes/Backup Macintosh HD/Users/chris/from_munkyii/ss-repo/svn/funknet-tools/trunk@73 94779f63-8a0d-0410-8a4e-8732221620d0

--- test-net-acl.pl
+++ test-net-acl.pl
@@ -3,17 +3,26 @@ use strict;
 use Net::ACL;
 use Net::ACL::Rule qw/ :action :rc /;
 use Net::ACL::RtConfig;
+use Data::Dumper;
 
-my $list_hr = load Net::ACL::RtConfig( dir => 'import',
-				       source_as => 'AS65000',
-				       peer_as => 'AS65002',
-				       source_addr => '10.2.0.37',
-				       peer_addr => '10.2.0.38'
-				     );
 
-my $list = renew Net::ACL( Type => 'prefix-list',
-			   Name => 4 
-			 );
+
+my $list_hr = Net::ACL::RtConfig->load( dir => 'export',
+					source_as => '65000',
+					peer_as => '65002',
+					source_addr => '10.2.0.37',
+					peer_addr => '10.2.0.38',
+					source => 'FUNKNET',
+					host => 'whois.funknet.org',
+					port => '43',
+					type => 'access-list',
+					protocol => 'ripe',
+					name => '65002export',
+				      );
+
+my $list = Net::ACL->renew( Type => 'access-list',
+			    Name => '65002export' 
+			  );
 
 my $config = $list->asconfig;
 
