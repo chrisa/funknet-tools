fa2a5f9b963fc312050a4cdd976aa95a841f2dc0
chris <chris@94779f63-8a0d-0410-8a4e-8732221620d0>
2003-06-23 12:38:23 +0000

    interface to Root.pm (albeit commented out for now) add docs
    git-svn-id: file:///Volumes/Backup Macintosh HD/Users/chris/from_munkyii/ss-repo/svn/funknet-tools/trunk@90 94779f63-8a0d-0410-8a4e-8732221620d0

--- lib/Funknet/Config/CommandSet.pm
+++ lib/Funknet/Config/CommandSet.pm
@@ -1,5 +1,38 @@
 package Funknet::Config::CommandSet;
 use strict;
+use Funknet::Config::CLI;
+use Funknet::Config::Root;
+
+=head1 NAME
+
+Funknet::Config::CommandSet
+
+=head1 DESCRIPTION
+
+A pair of classes to hold lists of commands, and a generic
+constructor. Class ::CommandSet::Host has an apply method which
+executes the commands as root on the local system. Class
+::CommandSet::CLI has an apply method which uses the CLI modules to
+execute commands on Zebra or IOS routers. Both will return their
+command lists with a text representation of where they should be
+executed, for preview and warnings. 
+
+=head1 CONSTRUCTOR
+
+Pass in the list of commands and the target. Also pass the static
+local_* values relevant. XXX -- this should come from ConfigFile.
+
+=head1 as_text
+
+Returns the list of commands, with a line describing where they should
+be executed. For notifying proposed changes.
+
+=head1 apply
+
+Runs the list of commands. XXX -- needs to gain root properly, not
+expect to be run as root.
+
+=cut
 
 sub new {
     my ($class, %args) = @_;
@@ -71,6 +104,16 @@ sub as_text {
 
 sub apply {
     my ($self) = @_;
+
+# New interface to Root.pm, not quite ready yet.
+
+#    my $root = Funknet::Config::Root->new;
+#    unless ($root) {
+#	die "can't get root";
+#    }
+#    my $rv = $root->exec_root( $self );
+#    return $rv;
+
     if (scalar @{ $self->{_cmds} }) {
         my $text = join "\n", @{ $self->{_cmds} };
 	qx[$text
@@ -79,7 +122,6 @@ sub apply {
     } else {
         return '';
     }
-
 }
 
 1;
