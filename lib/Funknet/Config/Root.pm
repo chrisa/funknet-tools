b320bc090461ada82a5b070a3f3d23927d29cacd
chris <chris@94779f63-8a0d-0410-8a4e-8732221620d0>
2003-06-23 12:35:49 +0000

    initial version; needs real sudo and userv stuff adding
    git-svn-id: file:///Volumes/Backup Macintosh HD/Users/chris/from_munkyii/ss-repo/svn/funknet-tools/trunk@89 94779f63-8a0d-0410-8a4e-8732221620d0

---
+++ lib/Funknet/Config/Root.pm
@@ -0,0 +1,61 @@
+package Funknet::Config::Root;
+use strict;
+
+=head1 NAME
+
+Funknet::Config::Root
+
+=head1 DESCRIPTION
+
+Class abstracting methods of getting root. 
+
+=head1 METHODS
+
+=head2 new
+
+=head2 exec_root
+
+=cut
+
+sub new {
+    my ($class) = @_;
+    my $self = bless {}, $class;
+
+    my $root_method = Funknet::Config::ConfigFile->root;
+
+    if ($root_method eq 'sudo') {
+	
+	$self->{_exec} = 
+	    sub ($) {
+		my ($cmd) = @_;
+		system "sudo $cmd";
+	    };
+
+    } elsif ($root_method eq 'userv') {
+
+	$self->{_exec} = 
+	    sub ($) {
+		my ($cmd) = @_;
+		system "userv $cmd"; # XXX this isn't right
+	    };
+
+    } elsif ($root_method eq 'runas') {
+
+	$self->{_exec} = 
+	    sub ($) {
+		my ($cmd) = @_;
+		system "$cmd";
+	    };
+
+    } else {
+	return undef;
+    }
+}
+
+sub exec_root {
+    my ($self, $cmdset) = @_;
+
+    for my $cmd ($cmdset->cmds) {
+	&{ $self->{_exec} } $cmd;
+    }
+}
