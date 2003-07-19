3ea5f67f9162d71e71595070f02f12c4cdf6c982
dunc <dunc@94779f63-8a0d-0410-8a4e-8732221620d0>
2003-07-19 23:59:34 +0000

    huagely different basically
    git-svn-id: file:///Volumes/Backup Macintosh HD/Users/chris/from_munkyii/ss-repo/svn/funknet-tools/trunk@205 94779f63-8a0d-0410-8a4e-8732221620d0

--- update_on_ip_change.pl
+++ update_on_ip_change.pl
@@ -1,6 +1,8 @@
-#!/usr/local/bin/perl -w
+#!/usr/pkg/bin/perl 
+
+#
 # Copyright (c) 2003
-#	The funknet.org Group.
+#       The funknet.org Group.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -12,8 +14,8 @@
 #    documentation and/or other materials provided with the distribution.
 # 3. All advertising materials mentioning features or use of this software
 #    must display the following acknowledgement:
-#	This product includes software developed by The funknet.org
-#	Group and its contributors.
+#       This product includes software developed by The funknet.org
+#       Group and its contributors.
 # 4. Neither the name of the Group nor the names of its contributors
 #    may be used to endorse or promote products derived from this software
 #    without specific prior written permission.
@@ -30,6 +32,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 
+
 # Script to check for change of IP on given Interface
 # compare to whois and update the whois if required.
 
@@ -40,124 +43,124 @@ use strict;
 use Net::Whois::RIPE;
 use Mail::GnuPG;
 use MIME::Entity;
+use Net::Interface;
+use Socket;
+use Getopt::Std;
 
-unless (scalar @ARGV == 2) 
-{
-	print STDERR "usage: $0 <interface> <tunnel_name>\n";
-	exit(1);
+use lib './lib';
+use Funknet::Config;
+
+use Data::Dumper;
+
+my %opt;
+getopts('f:i:', \%opt);
+unless ($opt{f} && $opt{i}) {
+    print STDERR "usage: $0 -f <path_to_funknet_config_file> -i <interface>\n";
+    exit(1);
 }
 
-my $update_email='auto-dbm@funknet.org';
-my $key_id='5CDA2963';
-my $signing_email='funknet@vogon.braddon.org.uk';
-my $key_passphrase='';
+my $config = Funknet::Config::ConfigFile->new( $opt{f} );
+my $nic = new Net::Interface($opt{i}) or die "whoopsy creating interface object";
+
+my $addr= $nic->address;
+my $ip_info = inet_ntoa($addr);
+
+# if IP has changed, we need 1st to update the funknet.conf file
+# as other bits use it 
+#if ($config->local_endpoint ne $ip_info)
+#{
+#	print STDERR "best update the funknet.conf\n";
+#	$config->{local_endpoint} = $ip_info;
+#	print Dumper $config;
+#	$config->write();
+#}
+
+my $update_email = $config->update_email;
+my $key_id = $config->pgp_key_id;
+my $pgp_passphrase = $config->pgp_passphrase;
+my $pgp_key_dir = $config->pgp_key_dir;
+my $signing_email = $config->signing_email;
+my $from_email = $config->from_email;
+my $local_as = $config->local_as;
 
 my $hostname = qx[/bin/hostname];
 
-my $iface = $ARGV[0];
-my $tunnel_object = $ARGV[1];
-
-my $old_ip = get_ip_from_whois($tunnel_object);
-my $ip_info = get_ip($iface);
-chomp($ip_info);
-
-if($old_ip ne $ip_info)
+my $whois = Funknet::Config::Whois->new;
+unless (defined $whois)
 {
-	print"IP changed from $old_ip to $ip_info\n";
-	update_whois($ip_info);
-}
-else
-{
-	print"IP same\n";
+	die "Error getting a  Funknet::Config::Whois object\n";
 }
 
+my @tunnel_objects = $whois->my_tunnels;
+
+if(!($local_as =~ /AS65\d\d\d/))
+{
+	print STDERR "invalid local_as : (format AS65xxx)\n";
+	exit(1);
+}
+
+foreach my $tunnel_object (@tunnel_objects)
+{
+	my $old_ip;
+        my @endpoints = $tunnel_object->endpoint;
+        my @ass = $tunnel_object->as;
+        my ($as1,$as2) = @ass;
+        if ($as1 eq $local_as)
+        {
+                 $old_ip = shift(@endpoints);
+        }
+        elsif ($as2 eq $local_as)
+        {
+                my $tmp = shift(@endpoints);
+                $old_ip = shift(@endpoints);
+        }
+	else
+	{
+		print STDERR "Error: Object $tunnel_object is not for $local_as\n";
+		next;
+	}
+
+	chomp($ip_info);
+
+	if($old_ip ne $ip_info)
+	{
+		print STDERR "IP changed from $old_ip to $ip_info\n";
+		update_whois($old_ip, $ip_info, $tunnel_object);
+	}
+	else
+	{
+		print"IP same\n";
+	}
+}
 exit(0);
-
-sub get_ip_from_whois
-{
-	my $tunnel_object = shift(@_);
-
-	my $whois = Net::Whois::RIPE->new('whois.funknet.org') || die "cant connect to whois";
-
-	$whois->no_recursive;
-	$whois->source('FUNKNET');
-	$whois->type('tunnel');
-
-	my $tun = $whois->query($tunnel_object);
-	my @endpoints = $tun->endpoint;
-	my $ip = shift(@endpoints);
-	chomp($ip);
-	return($ip);
-}
-
-sub read_file
-{
-	open(FILE,'current_ip');
-	my $old_ip = <FILE>;
-	close(FILE);
-	return($old_ip);
-}
-
-sub write_file
-{
-	my $new_ip = shift(@_);
-	open(FILE,'>current_ip') || die "couldn't write to IP cache file";
-	print(FILE "$new_ip");
-	close(FILE);
-}
-
-sub get_ip
-{
-	my $iface = shift(@_);
-
-	#This bit is shit, but nice module wouldn't build
-	my $ip = qx[/sbin/ifconfig $iface 2> /dev/null | /bin/grep inet | /bin/grep -v inet6] || die "couldn't get settings of interface $iface";
-	$ip =~ s/.*inet[^\d]+([^\ ]+).*/$1/;
-	return($ip);
-}
 
 sub update_whois
 {
+	my $old_ip = shift(@_);
 	my $new_ip = shift(@_);
-	print "updating whois to $new_ip\n";
-	my $subject="IP update from $hostname";
-	my @current_entry = get_entry_from_whois();
-	foreach my $thing (@current_entry)
+	my $current = shift(@_);
+	print STDERR "updating whois to $new_ip\n";
+	my @endpoints = $current->endpoint();
+	for my $ip (@endpoints)
 	{
-		$thing =~ s/$old_ip/$new_ip/;
-		$thing =~ s/changed:\s+.+@.+\s+\d+/changed: $signing_email/; 
-		$thing =~ s/^%.*//;
-		print "$thing\n";
-		$thing =~ s/^(.*)$/$1\n/;
+		if ($ip eq $old_ip) { $ip = $new_ip; }
 	}
-	my $ref_to_msg = \@current_entry;
-	my $mime_obj = MIME::Entity->build(From    => $signing_email,
+	$current->endpoint(\@endpoints);
+	$current->changed($signing_email);
+
+	my $entry = $current->text();
+	my $subject="IP Update from $hostname";
+	chomp($subject);
+	print"from:$from_email\nto:$update_email\nsubject:$subject\n";
+	my $mime_obj = MIME::Entity->build(From    => $from_email,
 					   To      => $update_email,
 					   Subject => $subject,
-					   Data    => $ref_to_msg);
-	print"$signing_email\n$update_email\n$subject\n$ref_to_msg\n$key_id\n";
+					   Data    => [$entry]);
 	my $mg = new Mail::GnuPG ( key => $key_id, 
-				   passphrase => $key_passphrase,
-				   keydir => '/home/funknet/.gnupg' );
+				   passphrase => $pgp_passphrase,
+				   keydir => $pgp_key_dir );
 	my $ret = $mg->mime_sign($mime_obj, $signing_email);
 	print"$ret\n";
 	$mime_obj->smtpsend;
 }
 
-sub get_entry_from_whois
-{
-	my $whois = Net::Whois::RIPE->new('whois.funknet.org') || die "cant connect to whois";
-
-	my @current;
-	my $test;
-
-	$whois->no_recursive;
-	$whois->source('FUNKNET');
-	my $iterator = $whois->query_iterator($tunnel_object);
-	while (my $obj = $iterator->next) 
-	{
-		$test = $obj->content;
-	}
-	@current=split('\n',$test);
-	return(@current);
-}
