11cce35ee1cec92c673694dbe3799d7350151ac0
doug <doug@94779f63-8a0d-0410-8a4e-8732221620d0>
2003-08-24 14:32:37 +0000

    does minimum stuff ok now
    git-svn-id: file:///Volumes/Backup Macintosh HD/Users/chris/from_munkyii/ss-repo/svn/funknet-tools/trunk@262 94779f63-8a0d-0410-8a4e-8732221620d0

--- create_nagios_config.pl
+++ create_nagios_config.pl
@@ -13,11 +13,15 @@ my @ass;
 my @as_names;
 my @done;
 my %nerd_autnum_objects;
-my %splurby_nerd_tunnels;
+my %blank_nerd_tunnels;
 my %as_names;
 my %as_nums;
 my %endpoints;
 my %nerd_tunnels;
+my $hosts;
+my $services;
+my $hosts_inside;
+my $hosts_outside;
 
 my $whois = Net::Whois::RIPE->new( 'whois.funknet.org');
 unless (defined $whois)
@@ -42,28 +46,29 @@ foreach my $thing ($transobj->members)
 }
 
 $whois->type('aut-num');
-my $splurby_aut_num = $whois->query('AS65000');
+my $blank_aut_num = $whois->query('AS65023');
 
-foreach my $thing ($splurby_aut_num->tun)
+foreach my $thing ($blank_aut_num->tun)
 {
 		$whois->type('tunnel');
 		my $tun = $whois->query($thing);
-		$splurby_nerd_tunnels{$thing} = $tun;		
+		$blank_nerd_tunnels{$thing} = $tun;		
 }
 
-foreach my $splurby_tun (keys(%splurby_nerd_tunnels))
+foreach my $blank_tun (keys(%blank_nerd_tunnels))
 {
-	my @endpoints = $splurby_nerd_tunnels{$splurby_tun}->endpoint;
-	my @address = $splurby_nerd_tunnels{$splurby_tun}->address;
-	my @ass = $splurby_nerd_tunnels{$splurby_tun}->as;
+	my @endpoints = $blank_nerd_tunnels{$blank_tun}->endpoint;
+	my @address = $blank_nerd_tunnels{$blank_tun}->address;
+	my @ass = $blank_nerd_tunnels{$blank_tun}->as;
 	my $ip;
 	my $ip_addy;
 	my $other_as;
 	my $as_name;
 	my $as_num;
-	my $splurby_as='AS65000';
+	my $blank_as='AS65023';
         my ($as1,$as2) = @ass;
-	if ($as1 =~ /AS65000/m)
+	next if ((($as1 eq 'AS65000') and ($as2 eq 'AS65023')) or (($as2 eq 'AS65000') and ($as1 eq 'AS65023')));
+	if ($as1 =~ /AS65023/m)
 	{ 
 		my $tmp = shift(@endpoints);
 		$ip = shift(@endpoints);
@@ -80,7 +85,8 @@ foreach my $splurby_tun (keys(%splurby_nerd_tunnels))
 	$endpoints{$other_as} = $ip;
 	$as_name=$as_names{$other_as};
 	$as_num=$as_nums{$as_name};
-	print qq[
+
+	$hosts .= qq[
 define host{
         host_name               $as_name
         alias                   $as_num
@@ -96,7 +102,7 @@ define host{
         host_name               ${as_name}-funknet
         alias                   $as_num
         address                 $ip_addy
-	parent_hosts		$as_name
+	parents			$as_name
         check_command           check_ping_remote
         checks_enabled          1
         max_check_attempts      5
@@ -105,6 +111,74 @@ define host{
         notification_options            d,u,r
         }
 ];
+	$services .= qq[
+define service{
+        host_name               $as_name
+        service_description     $as_name ping_remote
+        check_command           check_ping_remote 
+        max_check_attempts      5
+        check_period            24x7
+        normal_check_interval   15
+        retry_check_interval    1
+        notification_interval   30
+        notification_period     24x7
+        notification_options    w,c,r
+        contact_groups          funknet-outside
+        }
+define service{
+        host_name               ${as_name}-funknet
+        service_description     ${as_name}-funknet ping_remote
+        check_command           check_ping_remote 
+        max_check_attempts      5
+        check_period            24x7
+        normal_check_interval   15
+        retry_check_interval    1
+        notification_interval   30
+        notification_period     24x7
+        notification_options    w,c,r
+        contact_groups          funknet-inside
+        }
+];
 
+	$hosts_inside .= "${as_name}-funknet,";
+	$hosts_outside .= "$as_name,";
 }
+
+my $hostgroups_inside = qq[
+define hostgroup{
+        hostgroup_name          funknet-inside
+        alias                   funknet-inside
+        contact_groups          funknet-inside
+        members                 $hosts_inside
+}
+];
+my $hostgroups_outside = qq[
+define hostgroup{
+        hostgroup_name          funknet-outside
+        alias                   funknet-outside
+        contact_groups          funknet-outside
+        members                 $hosts_outside
+}
+];
+
+open(HOSTS,">hosts.cfg");
+print HOSTS qq[
+$hosts
+];
+close(HOSTS);
+
+open(SERVICES,">services.cfg");
+print SERVICES qq[
+$services
+];
+close(SERVICES);
+
+open(HOSTGROUPS,">hostgroups.cfg");
+print HOSTGROUPS qq[
+$hostgroups_inside
+$hostgroups_outside
+];
+close(HOSTS);
+
+exit 1;
 
