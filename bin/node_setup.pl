bfdec925a02012775b9e0a6b4bffc14d6070b9d4
chris <chris@94779f63-8a0d-0410-8a4e-8732221620d0>
2003-10-01 22:36:19 +0000

    use my person for a test
    git-svn-id: file:///Volumes/Backup Macintosh HD/Users/chris/from_munkyii/ss-repo/svn/funknet-tools/trunk@285 94779f63-8a0d-0410-8a4e-8732221620d0

--- bin/node_setup.pl
+++ bin/node_setup.pl
@@ -2,17 +2,23 @@
 use strict;
 use Funknet::Whois::ObjectGenerator;
 
+# get a blank generator object, and make an unmaintained person. 
+
 my $gen = Funknet::Whois::ObjectGenerator->new( source => 'FUNKNET' );
 my $me = $gen->person( 'name'    => 'Me',
-		       'address' => 'Some Where',
+		       'address' => [ 'Some', 'Where' ],
 		       'e_mail'  => 'me@example.com',
 		       'phone'   => '23785542312',
                        );
 
 print scalar $me->text;
 
-#my $gen = Funknet::Whois::ObjectGenerator->new( 'person' => 'ME1-FUNKNET' );
-#my $me = $gen->mntner( 'admin_c' => 'ME1-FUNKNET',
-#		       'tech_c'  => 'ME1-FUNKNET',
-#		       'name     => 'MY-MUNTER',
-#                       );
+# get a generator with that person, and make a maintainer. 
+
+my $gen = Funknet::Whois::ObjectGenerator->new('source' => 'FUNKNET', 
+					       'person' => 'CA1-FUNKNET' );
+
+my $me = $gen->mntner( 'name'    => 'MY-MUNTER', 'auth' => 'PGPKEY-B355A313',
+		       'descr'   => 'test munter', 'e_mail' => 'foo@bar.com' );
+
+print scalar $me->text;
