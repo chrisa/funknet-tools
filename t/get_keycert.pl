f6a179189081aa0637727054652cbfcbf6cd8e3a
chris <chris@94779f63-8a0d-0410-8a4e-8732221620d0>
2005-03-12 17:11:00 +0000

    simple test script for keystash client
    git-svn-id: file:///Volumes/Backup Macintosh HD/Users/chris/from_munkyii/ss-repo/svn/funknet-tools/trunk@645 94779f63-8a0d-0410-8a4e-8732221620d0

---
+++ t/get_keycert.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+use strict;
+use Funknet::KeyStash::Client;
+
+my $param = $ARGV[0];
+
+my $ks = Funknet::KeyStash::Client->new(
+					www_user     => 'foo',
+					www_pass     => 'bar',
+					www_host     => 'blank.netdotnet.net',
+					www_cert     => "/C=GB/O=Lemon Test/CN=blank.netdotnet.net",
+					www_ca       => "/C=GB/O=Lemon Test/CN=blank.netdotnet.net",
+					whois_host   => 'localhost',
+					whois_port   => 4343,
+					whois_source => 'FUNKNET',
+					path         => '/tmp/ks',
+				       );
+
+unless (defined $ks) {
+    warn "Couldn't get a KeyStash::Client";
+    exit 1;
+}
+
+my $cert = $ks->get_cert($param);
+if (!defined $cert) {
+    warn "certificate not found: $param";
+    return undef;
+}
+
+my $cn = $cert->owner;
+if (!defined $cn) {
+    $self "using cert filename for key filename: $param";
+    $cn = $param;
+}
+
+my $key = $ks->get_key($cn);
+if (!defined $key) {
+    warn "key not found: $cn";
+    exit 1;
+}
+
+print "OK";
