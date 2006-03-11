e5e8961c331b34f311e56e7b1b52443d5da7eb70
chris <chris@94779f63-8a0d-0410-8a4e-8732221620d0>
2006-03-11 17:16:14 +0000

    IPTables doesn't let you say "-s 0.0.0.0/0" (or -d) when deleting rules. extend the check we already had to include this case as well as "0.0.0.0"
    git-svn-id: file:///Volumes/Backup Macintosh HD/Users/chris/from_munkyii/ss-repo/svn/funknet-tools/trunk@854 94779f63-8a0d-0410-8a4e-8732221620d0

--- lib/Funknet/Config/FirewallRule/IPTables.pm
+++ lib/Funknet/Config/FirewallRule/IPTables.pm
@@ -102,7 +102,8 @@ sub _src {
     
     my $src_str = " ";
     if (defined $self->{_source_address} &&
-	$self->{_source_address} ne '0.0.0.0') {
+	$self->{_source_address} ne '0.0.0.0' &&
+        $self->{_source_address} ne '0.0.0.0/0') {
 	$src_str .= "-s $self->{_source_address} ";
     }
     return $src_str;
@@ -113,7 +114,8 @@ sub _dst {
     
     my $dst_str = " ";
     if (defined $self->{_destination_address} && 
-	$self->{_destination_address} ne '0.0.0.0') {
+	$self->{_destination_address} ne '0.0.0.0' &&
+	$self->{_destination_address} ne '0.0.0.0/0') {
 	$dst_str .= "-d $self->{_destination_address} ";
     }
     return $dst_str;
